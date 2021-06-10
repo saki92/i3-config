c5663ad83d0122a0f88683da2ab73052a972b26b
Sakthivel Velumani <sakthi92@gmail.com>
2021-06-10 22:18:56 +0200

    working fine with legion ubuntu 20.04

--- fuzzy_lock.sh
+++ fuzzy_lock.sh
@@ -9,5 +9,8 @@ mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
 # Lock screen displaying this image.
 i3lock -i /tmp/screen_locked.png
 
+# Remove the image
+rm /tmp/screen_locked.png
+
 # Turn the screen off after a delay.
 sleep 60; pgrep i3lock && xset dpms force off
--- i3exit.sh
+++ i3exit.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 lock() {
-    i3lock --color=000000
+    scrot /tmp/screen_locked.png
+    mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
+    i3lock -i /tmp/screen_locked.png
+    rm /tmp/screen_locked.png
 }
 
 case "$1" in
