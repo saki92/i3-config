4aad60e6659c0f4e13e987e2a3452a97b15740fd
Sakthivel Velumani <velumani@eurecom.fr>
2022-11-04 15:56:11 -0400

    last config on lenovo legion

--- eDP_only.sh
+++ eDP_only.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output HDMI1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output DP-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output HDMI1 --off --output DP2 --off --dpi 96
