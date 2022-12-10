11592ebea6949897b353d018995c9ef8aa8d0750
Sakthivel Velumani <velumani@eurecom.fr>
2022-12-10 07:13:38 +0530

    installed and working

--- fuzzy_lock.sh
+++ fuzzy_lock.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -e
 
 # Take a screenshot
-scrot /tmp/screen_locked.png
+#scrot /tmp/screen_locked.png
 
 # Pixellate it 10x
-mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
+#mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
 
 # Lock screen displaying this image.
-i3lock -i /tmp/screen_locked.png
+i3lock --color 000000
 
 # Remove the image
-rm /tmp/screen_locked.png
+#rm /tmp/screen_locked.png
 
 # Turn the screen off after a delay.
 sleep 60; pgrep i3lock && xset dpms force off
